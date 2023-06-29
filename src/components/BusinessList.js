7d6f014dabd3c4a7a6422c625efe4c19b4325a6f
joaovian06 <joaovitor_vian@hotmail.com>
2023-06-28 21:09:45 -0300

    feat: create BusinessList component

---
+++ src/components/BusinessList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Business from './Business';
+
+class BusinessList extends React.Component {
+  
+  render() {
+    return (
+      <>
+        <Business></Business>
+        <Business></Business>
+        <Business></Business>
+        <Business></Business>
+      </>
+    )
+  }
+}
