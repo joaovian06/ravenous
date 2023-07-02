40dd7d338bc04b4c9c8be7299a49940d9f40474b
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-02 11:36:24 -0300

    feat: finish last part of ravenous project sample

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,25 @@
-import styles from './App.css';
-import SearchBar from '../SearchBar/SearchBar';
-import BusinessList from '../BusinessList/BusinessList';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
-const business = {
-  imageSrc:
-    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
-  name: "MarginOtto Pizzeria",
-  address: "1010 Paddington Way",
-  city: "Bordertown",
-  state: "NY",
-  zipCode: "10101",
-  category: "Italian",
-  rating: 4.5,
-  reviewCount: 90,
-};
+import BusinessList from "../BusinessList/BusinessList";
+import SearchBar from "../SearchBar/SearchBar";
+import Yelp from "../../utils/Yelp";
 
-const businesses = [business, business, business, business]
+const App = () => {
+  const [businesses, setBusinesses] = useState([]);
 
-function App() {
+  const searchYelp = async (term, location, sortBy) => {
+    const businesses = await Yelp.search(term, location, sortBy);
+    setBusinesses(businesses);
+  };
+
   return (
     <div className={styles.App}>
-      <SearchBar></SearchBar>
-      <BusinessList businesses={businesses}></BusinessList>
+      <h1>ravenous</h1>
+      <SearchBar searchYelp={searchYelp} />
+      <BusinessList businesses={businesses} />
     </div>
   );
-}
+};
 
 export default App;
