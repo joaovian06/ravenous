8f2dd72b50026cff2149d6f3271c32812aaac014
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-02 00:42:42 -0300

    finishing ravenous part 3

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./SearchBar.module.css";
 
 const sortByOptions = {
@@ -7,25 +7,59 @@ const sortByOptions = {
   "Most Reviewed": "review_count",
 };
 
-const SearchBar = () => {
+const SearchBar = (props) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [location, setLocation] = useState('');
+  const [sortingOption, setSortingOption] = useState('');
+
+  const onSearchChange = (event) => {
+    setSearchTerm({ searchTerm: event.target.value });
+  }
+
+  const onLocationChange = (event) => {
+    setLocation({ location: event.target.value });
+  }
+
+  const onSortingOptionClick = (event) => {
+    setSortingOption({ sortingOption: event.target.value });
+  }
+
   const renderSortByOptions = () => {
     return Object.keys(sortByOptions).map((sortByOption) => {
       let sortByOptionValue = sortByOptions[sortByOption];
-      return <li key={sortByOptionValue}>{sortByOption}</li>;
+      return <li value={sortingOption} onClick={onSortingOptionClick} key={sortByOptionValue}>{sortByOption}</li>;
     });
   };
+
+  const onSearchSubmit = (event) => {
+    console.log({
+      searchTerm: searchTerm,
+      location: location,
+      sortingOption: sortingOption
+    })
+  }
 
   return (
     <div className={styles.SearchBar}>
       <div className={styles.SearchBarSortOptions}>
-        <ul>{renderSortByOptions()}</ul>
+        <ul >{renderSortByOptions()}</ul>
       </div>
       <div className={styles.SearchBarFields}>
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input
+          type="text"
+          placeholder="Search Term"
+          value={searchTerm}
+          onChange={onSearchChange}
+        />
+        <input
+          type="text"
+          placeholder="Location"
+          value={location}
+          onChange={onLocationChange}
+        />
       </div>
       <div className={styles.SearchBarSubmit}>
-        <a>Let's Go</a>
+        <a onClick={onSearchSubmit}>Let's Go</a>
       </div>
     </div>
   );
