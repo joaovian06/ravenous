64d77beaa36bfd3ff09fd7d30b4cc8723a3ec4a4
joaovian06 <joaovitor_vian@hotmail.com>
2023-07-01 22:14:10 -0300

    feat: passing props throught components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,27 @@
-import './App.css';
+import styles from './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
 
+const business = {
+  imageSrc:
+    "https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg",
+  name: "MarginOtto Pizzeria",
+  address: "1010 Paddington Way",
+  city: "Bordertown",
+  state: "NY",
+  zipCode: "10101",
+  category: "Italian",
+  rating: 4.5,
+  reviewCount: 90,
+};
+
+const businesses = [business, business, business, business]
+
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <SearchBar></SearchBar>
-      <BusinessList></BusinessList>
+      <BusinessList businesses={businesses}></BusinessList>
     </div>
   );
 }
